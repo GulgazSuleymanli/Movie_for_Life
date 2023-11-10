91a361d27f748a7016f01d557e53237b05a67db7
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2023-11-10 12:35:50 +0400

    MovieforLife Demo

--- MforL_Front/assets/details.js
+++ MforL_Front/assets/details.js
@@ -1,5 +1,100 @@
-let movies = document.querySelector(".movies")
+let details = document.querySelector(".details")
 
-fetch("https://api.tvmaze.com/shows/1")
+let id = document.location.search.substring(4)
+
+fetch("https://api.tvmaze.com/shows/"+id)
     .then(res => res.json())
-    .then(datas =>console.log(datas))
+    .then(data =>{
+        details.innerHTML += `
+        <div class="col-3">
+            <div class="card" style="width: 18rem;">
+                <img src="${data.image.medium}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a href="${data.officialSite}"><h5 class="card-title">${data.name}</h5></a>
+                </div>
+            </div>
+        </div>
+        `
+
+        details.innerHTML += `
+            <div class="col-9">
+                <h5>Overview:</h5>
+                <p>${data.sumary}</p>
+                <strong>Genres:</strong>` + 
+                data.genres.forEach(element => {
+                    `<p d-flex>${element},</p>`
+                }) +
+                `<table class="table">
+                    <tr>
+                        <th>Average Runtime</th>
+                        <th>Language</th>
+                        <th>Country</th>
+                        <th>Rating</th>
+                        <th>Premiered</th>
+                        <th>Status</th>
+                    </tr>
+                    <tr>
+                        <td>${data.averageRuntime}</td>
+                        <td>${data.language}</td>
+                        <td>${data.network.country.code}</td>
+                        <td>${data.rating.average}</td>
+                        <td>${data.premiered}</td>
+                        <td>${data.status}</td>
+                    </tr>
+                </table>
+            </div>
+        `
+
+
+
+
+        // let maindiv = document.createElement("div")
+        // maindiv.classList.add("col-9")
+
+        // maindiv.innerHTML = `
+        //     <h5>Overview:</h5>
+        //     <p>${data.sumary}</p>
+        //     <strong>Genres:</strong>` +
+            // data.genres.forEach(element => {
+            //     `<p d-flex>${element},</p>`
+            // })
+        
+
+
+        // let table = document.createElement("table")
+        // const particulars = ["Average Runtime", "Language", "Country", "Rating", "Premier", "Status"]
+        
+        // let trH = document.createElement("tr")
+        
+        // particulars.forEach(p=>{
+        //     let th = document.createElement("th")
+        //     th.innerHTML = p
+        //     trH.appendChild(th)
+        // })
+
+        // table.appendChild(tr)
+
+        // let trD = document.createElement("tr")
+
+        // let tdAR = document.createElement("td")
+        // tdAR.innerHTML = data.averageRuntime
+        // let tdL = document.createElement("td")
+        // tdL.innerHTML = data.language
+        // let tdC = document.createElement("td")
+        // tdC.innerHTML = data.network.country.code
+        // let tdR = document.createElement("td")
+        // tdR.innerHTML = data.rating.average
+        // let tdP = document.createElement("td")
+        // tdP.innerHTML = data.premiered
+        // let tdS = document.createElement("td")
+        // tdS.innerHTML = data.status
+
+        // trD.append(tdAR, tdL. tdC, tdR, tdP, tdS)
+
+        // table.appendChild(trD)
+
+        // maindiv.appendChild(table)
+
+        // details.appendChild(maindiv)
+
+    })
